b567fb3504bcb572b15cca9544355db149fef103
zhangyunan <zyndev@gmail.com>
2020-09-19 16:09:35 +0800

    cors

--- flask-demo/app.py
+++ flask-demo/app.py
@@ -1,1 +1,15 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+
+@app.route('/')
+@cross_origin(supports_credentials=True)
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {name}!'
+
+
+app.run()
